ca0112ccce64c46a1e9601b609cd1bc62b640355
flizzyflo <vip3r1@gmx.de>
2022-06-21 00:53:01 +0200

    commented exceptions

--- doubleKeyException.java
+++ doubleKeyException.java
@@ -2,6 +2,8 @@ package Dict;
 
 public class doubleKeyException extends Exception {
 
+    // Is thrown when a key which already exists is tried to added again. Keys are unique.
+
     doubleKeyException(String errorMessage){
         super(errorMessage);
     }
--- missingKeyException.java
+++ missingKeyException.java
@@ -1,6 +1,8 @@
 package Dict;
 
 public class missingKeyException extends Exception{
+
+    // Exception is thrown when a specific key is looked up, but does not exist within the dictionary.
     
     missingKeyException(String errorMessage){
         super(errorMessage)
